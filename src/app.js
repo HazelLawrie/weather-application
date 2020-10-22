c8fa17c9f773a5f5e4b3dde8fdd5b122554d50a0
Hazel Lawrie <71905801+HazelLawrie@users.noreply.github.com>
2020-10-22 16:51:13 +0100

    Added Default Weather in JS for London upon opening webpage

--- src/app.js
+++ src/app.js
@@ -40,34 +40,39 @@ function currentDate() {
     minutes = `0${minutes}`;
   }
   let currentDateTime = document.querySelector("#date");
-  currentDateTime.innerHTML = `${day}, ${dateOfMonth} ${month} ${year} <br /> ${hours}:${minutes}`;
+  currentDateTime.innerHTML = `${day}, ${dateOfMonth} ${month} ${year} <br /> <small>${hours}:${minutes}</small>`;
 }
 
 currentDate();
 
-// Show Weather and City on Screen
+// Show Weather and City on Screen after fetching Geolocation
 
 function showWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
-  let temperature = Math.round(response.data.main.temp);
-  document.querySelector("#displayed-temp").innerHTML = `${temperature}`;
+  document.querySelector("#displayed-temp").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#temp-description").innerHTML = response.data.weather[0].main;
+  document.querySelector("#pressure-value").innerHTML = response.data.main.pressure;
+  document.querySelector("#humidity-value").innerHTML = response.data.main.humidity;
+  console.log(response);
+  retrieveImperialData();
 }
 
-// Search for City
+function showWind(response) {
+document.querySelector("#wind-value").innerHTML = `${Math.round(response.data.wind.speed)}`;
+console.log(response);
+}
+// Show Wind in MPH rather than default m/s 
 
-function searchLocation(city) {
+function retrieveImperial(position) {
   let apiKey = "1dca542b443d294d157be34aefdc0627";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = `imperial`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+  axios.get(url).then(showWind);
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#input").value;
-  city.innerHTML = `${input.value}`;
-}
-
-// Geolocation
+// Geolocation when clicking location button
 
 function retrievePosition(position) {
   let apiKey = "1dca542b443d294d157be34aefdc0627";
@@ -79,12 +84,49 @@ function retrievePosition(position) {
 }
 
 function retreiveGeolocation(event) {
-event.preventDefault();
+  event.preventDefault();
   navigator.geolocation.getCurrentPosition(retrievePosition);
 }
 
-let searchForm = document.querySelector("#input");
-searchForm.addEventListener("submit", handleSubmit);
+function retrieveImperialData() {
+  navigator.geolocation.getCurrentPosition(retrieveImperial);
+  currentDate();
+}
 
 let locationButton = document.querySelector("#location-button");
 locationButton.addEventListener("click", retreiveGeolocation);
+
+// Default Real Weather Data for London Shown when Opening Page
+
+function autoShowWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#displayed-temp").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#temp-description").innerHTML = response.data.weather[0].main;
+  document.querySelector("#pressure-value").innerHTML = response.data.main.pressure;
+  document.querySelector("#humidity-value").innerHTML = response.data.main.humidity;
+  console.log(response);
+}
+
+// Show Wind in MPH rather than default m/s
+
+function autoShowWind(response) {
+  document.querySelector("#wind-value").innerHTML = Math.round(response.data.wind.speed);
+  console.log(response);
+}
+
+function autoRetrieveLondon() {
+  let apiKey = "1dca542b443d294d157be34aefdc0627";
+  let units = `metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=${units}&appid=${apiKey}`;
+  axios.get(url).then(autoShowWeather);
+}
+
+function autoRetrieveLondonImperial() {
+  let apiKey = "1dca542b443d294d157be34aefdc0627";
+  let units = `imperial`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=${units}&appid=${apiKey}`;
+  axios.get(url).then(autoShowWind);
+}
+
+autoRetrieveLondon();
+autoRetrieveLondonImperial();
